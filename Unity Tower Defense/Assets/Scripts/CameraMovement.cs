0868fd15b9719ebddef801da810420c84af5c159
jpmancini <jpmancini623@gmail.com>
2020-05-16 17:26:48 -0400

    Added CameraMovement script

---
+++ Unity Tower Defense/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float cameraSpeed = 0;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        GetInput();
+    }
+
+    private void GetInput()
+    {
+      if (Input.GetKey(KeyCode.W))
+      {
+        transform.Translate(Vector3.up * cameraSpeed * Time.deltaTime);
+      }
+      if (Input.GetKey(KeyCode.A))
+      {
+        transform.Translate(Vector3.left * cameraSpeed * Time.deltaTime);
+      }
+      if (Input.GetKey(KeyCode.S))
+      {
+        transform.Translate(Vector3.down * cameraSpeed * Time.deltaTime);
+      }
+      if (Input.GetKey(KeyCode.D))
+      {
+        transform.Translate(Vector3.right * cameraSpeed * Time.deltaTime);
+      }
+    }
+
+}
