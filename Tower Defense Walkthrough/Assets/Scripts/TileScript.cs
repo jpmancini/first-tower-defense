376bf9f95123863c9a240b7449d101b569d324e7
Danny Gletner <djg91@georgetown.edu>
2020-06-01 15:57:08 -0400

    can place a tower on click

--- Tower Defense Walkthrough/Assets/Scripts/TileScript.cs
+++ Tower Defense Walkthrough/Assets/Scripts/TileScript.cs
@@ -28,13 +28,26 @@ public class TileScript : MonoBehaviour
     }
 
     public void Setup(Point gridPos, Vector3 worldPos, Transform parent)
-    { 
+    {
         this.GridPosition = gridPos;
         transform.position = worldPos;
         transform.SetParent(parent);
-        
+
 
         LevelManager.Instance.Tiles.Add(gridPos, this);
 
+    }
+
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            PlaceTower();
+        }   
+    }
+
+    private void PlaceTower()
+    {
+        Instantiate(GameManager.Instance.TowerPrefab, transform.position, Quaternion.identity);
     }
 }
