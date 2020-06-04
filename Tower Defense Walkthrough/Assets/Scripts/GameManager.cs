9363642aaae7562647cd954cf019a762807eb2c1
Danny Gletner <djg91@georgetown.edu>
2020-06-04 18:07:20 -0400

    Currency indicator added

--- Tower Defense Walkthrough/Assets/Scripts/GameManager.cs
+++ Tower Defense Walkthrough/Assets/Scripts/GameManager.cs
@@ -1,15 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : Singleton<GameManager> 
 {
     public TowerBtn ClickedBtn { get; set; }
 
+    private int currency;
+
+    public int Currency
+    {
+        get
+        {
+            return currency;
+        }
+        set
+        {
+            this.currency = value;
+            this.currencyTxt.text = "$" + value.ToString();
+        }
+    }
+
+    [SerializeField]
+    private Text currencyTxt; 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Currency = 5;
     }
 
     // Update is called once per frame
